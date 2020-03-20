2843d09cf0409d78c30a2d951737dc15cff9b15d
Leon Li <31671708+leonli33@users.noreply.github.com>
2020-03-19 21:44:58 -0700

    Added a slider

--- src/SortingVisual/SortingMain.js
+++ src/SortingVisual/SortingMain.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import './SortingMain.css';
 import Bar from './Bar'
+// message to Grev: Happy coding! YOU CAN DO IT 
+
 
 export default class SortingMain extends Component {
     constructor(props) {
@@ -8,7 +10,8 @@ export default class SortingMain extends Component {
         this.state = {
             currentAlgo: "Merge Sort",
             algorithms: [],
-            elementsToSort: []
+            elementsToSort: [],
+            numberOfElements: 55
         }
     }
 
@@ -17,20 +20,34 @@ export default class SortingMain extends Component {
         return Math.floor(Math.random() * Math.floor(max));
     }
 
+    // i am trying to update the width and height of bar being displayed based on how many
+    // elements there are. This code is not functional. Feel free to change any of it or delete
+    // it if there is a better way to do it :)
     componentDidUpdate() {
-        for(let i = 0; i < 30;i++) {
+        let totalWidth = window.screen.width;
+        let width = (totalWidth - (this.state.numberOfElements * 16))/this.state.numberOfElements;
+        for(let i = 0; i < this.state.numberOfElements;i++) {
             let element = document.getElementById("Bar-" + i);
             element.style.height = this.state.elementsToSort[i] * 8 + "px";
+            element.style.width = width + "px";
         }
     }
 
+    handleNumElementChange = (event) => {
+        // set the number of elements in the array to the value on slider
+        this.setState({
+            numberOfElements: event.target.value
+        });
+    }
+
     componentDidMount() {
+        // initialize elements
         let algos = ["Merge Sort", "Quick Sort", "Insertion Sort", "Bubble Sort","Shell Sort",
                     "Tim Sort","Radix Sort","Tree Sort"];
         let elements =[];
-        // initialize the array with 30 elements of random value
-        for(let i = 0; i < 30; i++) {
-            let num = this.getRandomInt(50) + 5;
+        // initialize the array with 30 elements of random values
+        for(let i = 0; i < this.state.numberOfElements; i++) {
+            let num = this.getRandomInt(50);
             elements.push(num);
         }
         this.setState({
@@ -40,9 +57,10 @@ export default class SortingMain extends Component {
     }
 
     handleRandomizeClick = () => {
+        // randomize elements
         let elements = [];
-        for(let i = 0; i < 30; i++) {
-            let num = this.getRandomInt(50) + 5;
+        for(let i = 0; i < this.state.numberOfElements; i++) {
+            let num = this.getRandomInt(50);
             elements.push(num);
         }
         this.setState({
@@ -51,6 +69,7 @@ export default class SortingMain extends Component {
     }
 
     updateCurrentAlgo = (event) => {
+        // set the current algo selected
         this.setState({
             currentAlgo: event.target.value
         })
@@ -76,14 +95,15 @@ export default class SortingMain extends Component {
                 </div>
                 <div className="footer">
                     <label className="label">Sort with: </label>
-                    <select className="DropDown" value={this.state.currentAlgo} onChange={this.updateCurrentAlgo}>
+                    <select className="dropDown" value={this.state.currentAlgo} onChange={this.updateCurrentAlgo}>
                         {this.state.algorithms.map(algorithms => (
                             <option key={algorithms} value={algorithms}>
                                 {algorithms}
                             </option>
                         ))}
                     </select>
-                    <label className="label">Number of Elements: (implement slider bar here)</label>
+                    <label className="label">Number of Elements:</label>
+                    <input type="range" min="0" max="100" className="slider" onChange={this.handleNumElementChange}></input>
                     <button className="button">Visualize Sorting</button>
                     <button className="button" onClick={this.handleRandomizeClick}>Randomize Elements</button>
                     <button className="button">Reset</button>
